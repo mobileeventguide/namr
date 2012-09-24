45653f0c39415989412c61698f2fa742235e216b
Nick Thomas <nick.thomas@mobileeventguide.de>
2012-09-24 11:01:02 +0200

    first version usable

--- README.md
+++ README.md
@@ -2,3 +2,28 @@ namr
 ====
 
 Memorisable Name Generator for API Keys
+
+functions
+---------
+<pre>
+NAMR::Generator.capitalized_identifier
+ => "UnhallowedDagothUr" 
+ => "CuteElectivire" 
+ => "GleefulAres" 
+ => "FlavoryShangTsung" 
+ => "BlitheBlastoise" 
+ => "DelectablePidgeot" 
+ => "GrossCelebi" 
+</pre>
+
+<pre>
+NAMR::Generator.separated_identifier
+ => "imposing-samurott" 
+ => "squalid-haunter" 
+ => "content-raikou" 
+ => "flavory-laptop-gun" 
+ => "alarming-aerodactyl" 
+ => "sensational-savage-dragon" 
+ => "disagreeable-bionic-arm" 
+ => "gleeful-experimental-mirv" 
+</pre>
--- lib/namr.rb
+++ lib/namr.rb
@@ -1,9 +1,39 @@
-require "namr/version"
+#require "namr/version"
 
 module NAMR
-   class Generator
-     def self.level_one
-       File.open("./levels/level1.txt")
-     end
-   end
+  class Generator
+    def self.object_all
+      File.open("./lib/levels/object.txt").read.split("\n")
+    end
+
+    def self.pick_object
+      os = self.object_all
+      os[rand(os.length-1)]
+    end
+
+    def self.adjective_all
+      File.open("./lib/levels/adjective.txt").read.split("\n")
+    end
+
+    def self.pick_adjective
+      as = self.adjective_all
+      as[rand(as.length-1)]
+    end
+
+    def self.identifier
+      [self.pick_adjective, self.pick_object].join(" ")
+    end
+
+    def self.safe_identifier
+      self.identifier.downcase.gsub(/[^0-9a-z ]/i, '').gsub(/ +/, ' ')
+    end
+
+    def self.capitalized_identifier
+      self.safe_identifier.split(" ").map(&:capitalize).join('')
+    end
+    
+    def self.separated_identifier
+      self.safe_identifier.gsub(' ','-')
+    end
+  end
 end
